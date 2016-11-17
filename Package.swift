6959858707f2391df83ae78aa59555ff4d658f30
Jonathan Guthrie <jono@perfect.org>
2016-11-17 11:36:58 -0500

    add logger and random

--- Package.swift
+++ Package.swift
@@ -1,8 +1,12 @@
+// Generated automatically by Perfect Assistant Application
+// Date: 2016-11-17 15:50:06 +0000
 import PackageDescription
-
 let package = Package(
     name: "PerfectRequestLogger",
-	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0)
-	]
+    targets: [],
+    dependencies: [
+        Package.Dependency.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 0),
+        Package.Dependency.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
+        Package.Dependency.Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0, minor: 2),
+    ]
 )
