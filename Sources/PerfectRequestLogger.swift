643f933d387396d29a1dfc606bcb4fabc1e3c46e
Jonathan Guthrie <jono@perfect.org>
2016-11-17 11:37:34 -0500

    updating to work on Linux, and to log to console AND log file.

--- Sources/PerfectRequestLogger.swift
+++ Sources/PerfectRequestLogger.swift
@@ -1,16 +1,22 @@
 import PerfectLib
 import PerfectHTTP
 import PerfectNet
-import Foundation
+import SwiftMoment
+import PerfectLogger
+import SwiftRandom
+
+public var requestLogFileLocation = "/var/log/perfectLog.log"
 
 public class RequestLogger: HTTPRequestFilter, HTTPResponseFilter {
+
+	var defaultLogFile = "/var/log/perfectLog.log"
 
 	var randomID: String
 	var sequence: UInt32
 
 	public init() {
 		// Generate random string to prefix request IDs
-		randomID = randomAlphaNumericString(length: 8)
+		randomID = Randoms.randomAlphaNumericString(length: 8)
 		// Initialize a request count
 		sequence = 0
 	}
@@ -19,7 +25,7 @@ public class RequestLogger: HTTPRequestFilter, HTTPResponseFilter {
 	public func filter(request: HTTPRequest, response: HTTPResponse, callback: (HTTPRequestFilterResult) -> ()) {
 
 		// Store request start time
-		request.scratchPad["start"] = Date()
+		request.scratchPad["start"] = moment()
 
 		// Store a unique request ID, this can be used in other logging to correlate to the request log
 		sequence += 1
@@ -35,15 +41,19 @@ public class RequestLogger: HTTPRequestFilter, HTTPResponseFilter {
 		let method = response.request.method
 		let requestURL = response.request.uri
 		let remoteAddress = response.request.remoteAddress.host
-		let start = response.request.scratchPad["start"] as! Date
+		let start = response.request.scratchPad["start"] as! Moment
 		let protocolVersion = response.request.protocolVersion
 		let status = response.status.code
 		let length = response.bodyBytes.count
 		let requestProtocol = response.request.connection is PerfectNet.NetTCPSSL ? "HTTPS" : "HTTP"
 
-		let interval = start.timeIntervalSinceNow * -1
+//		let interval = start.timeIntervalSinceNow * -1
+		let interval = start.intervalSince(moment()).seconds * -1
 
-		Log.info(message: "[\(hostname)/\(requestID)] \(start) \"\(method) \(requestURL) \(requestProtocol)/\(protocolVersion.0).\(protocolVersion.1)\" from \(remoteAddress) - \(status) \(length)B in \(interval)s")
+		var useFile = requestLogFileLocation
+		if useFile.isEmpty { useFile = defaultLogFile }
+
+		LogFile.info("[\(hostname)/\(requestID)] \(start) \"\(method) \(requestURL) \(requestProtocol)/\(protocolVersion.0).\(protocolVersion.1)\" from \(remoteAddress) - \(status) \(length)B in \(interval)s", useFile)
 
 		callback(.continue)
 	}
@@ -52,21 +62,4 @@ public class RequestLogger: HTTPRequestFilter, HTTPResponseFilter {
 	public func filterBody(response: HTTPResponse, callback: (HTTPResponseFilterResult) -> ()) {
 		callback(.continue)
 	}
-}
-
-// http://stackoverflow.com/questions/26845307/generate-random-alphanumeric-string-in-swift
-func randomAlphaNumericString(length: Int) -> String {
-
-	let allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-	let allowedCharsCount = UInt32(allowedChars.characters.count)
-	var randomString = ""
-
-	for _ in 0..<length {
-		let randomNum = Int(arc4random_uniform(allowedCharsCount))
-		let randomIndex = allowedChars.index(allowedChars.startIndex, offsetBy: randomNum)
-		let newCharacter = allowedChars[randomIndex]
-		randomString += String(newCharacter)
-	}
-
-	return randomString
 }
