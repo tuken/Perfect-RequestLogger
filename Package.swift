9ac0ed5821629fe480c19d60cbae7ceffc5eada5
Jonathan Guthrie <jono@perfect.org>
2017-01-05 12:20:49 -0500

    update http server revision

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-11-17 15:50:06 +0000
+// Date: 2017-01-05 17:19:39 +0000
 import PackageDescription
 let package = Package(
     name: "PerfectRequestLogger",
     targets: [],
     dependencies: [
-        Package.Dependency.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 0),
-        Package.Dependency.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
-        Package.Dependency.Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", versions: Version(0,0,0)..<Version(0,9223372036854775807,9223372036854775807)),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 1),
+        .Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0, minor: 2),
     ]
 )
