4ee6fcdc02285309bf0bca8429ff28e0945fd0b0
Kyle Jessup <badthing@gmail.com>
2017-06-02 10:41:27 -0400

    Removed minor version specs

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-01-12 15:37:07 +0000
+// Date: 2017-06-02 14:35:54 +0000
 import PackageDescription
 let package = Package(
-    name: "PerfectRequestLogger",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 1),
-        .Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0, minor: 2),
-    ]
+	name: "PerfectRequestLogger",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
+		.Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0),
+	]
 )
