a2bb96ed5b22246d839513b278086ecb94dfa901
Jonathan Guthrie <jono@perfect.org>
2017-09-29 14:12:00 -0400

    update to swift 4 perfect 3

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ let package = Package(
 	name: "PerfectRequestLogger",
 	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 1),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Logger.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
 		.Package(url: "https://github.com/iamjono/SwiftRandom.git", majorVersion: 0),
 	]
 )
